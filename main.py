dd263fd14f2eec27651d62096ced8be52f436fee
PpurpleHaze <moyazmoyaz005@gmail.com>
2026-03-03 15:27:21 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from admin_module import ensure_file, PLOTS_FILE, MEMBERS_FILE, BOOKINGS_FILE, PAYMENTS_FILE, admin_menu
 from plot_manager_module import plot_manager_menu
 from member_module import member_menu
+from maintenance_staff import update_plot_status
 
 def main_menu():
     while True:
-        print("\n === Community Garden Management System===")
+        print("\n === Community Garden Management System==")
         print("1. Garden Administrator")
         print("2. Plot Manager")
         print("3. Garden Member")
+        print("4. Maintenance ")
         print("0. Exit")
         choice = input("Enter choice: ").strip()
 
@@ -17,11 +19,13 @@ def main_menu():
             plot_manager_menu()
         elif choice == "3":
             member_menu()
+        elif choice == "4":
+            update_plot_status()
         elif choice == "0":
             print("Thank you for using GreenThumb Gardens system!")
             break
         else:
-            print("Invalid option. Please enter a choice from 0, 1, 2, or 3.")
+            print("Invalid option. Please enter a choice from 0, 1, 2, 3 or 4.")
 
 if __name__ == "__main__":
     print("Initializing files")
